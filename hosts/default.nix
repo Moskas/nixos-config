83291ac6efec55489b4b8e30ac8259b816a7d386
Moskas <minemoskas@gmail.com>
2023-06-28 12:20:34 +0200

    Testing dynamic import

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,4 +1,4 @@
-{ lib, nixpkgs, home-manager, username, ... }:
+{ lib, inputs, nixpkgs, home-manager, username, ... }:
 
 let
   system = "x86_64-linux";
@@ -8,12 +8,25 @@ let
   };
   lib = nixpkgs.lib;
 in {
-  virtual = nixpkgs.lib.nixosSystem {
-    modules = [ ./hosts/virtual/configuration.nix ];
+  virtual = lib.nixosSystem {
+    inherit system;
+    specialArgs = { inherit inputs username; };
+    modules = [
+      ./virtual
+      ./configuration.nix
+      home-manager.nixosModules.home-manager
+      {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.extraSpecialArgs = { inherit username; };
+        home-manager.users.${username}.imports =
+          [ (import ./virtual/home.nix) ];
+      }
+    ];
   };
-  omen =
-    nixpkgs.lib.nixosSystem { modules = [ ./hosts/omen/configuration.nix ]; };
-  gungnir = nixpkgs.lib.nixosSystem {
-    modules = [ ./hosts/gungnir/configuration.nix ];
-  };
+  #omen =
+  #  nixpkgs.lib.nixosSystem { modules = [ ./hosts/omen/configuration.nix ]; };
+  #gungnir = nixpkgs.lib.nixosSystem {
+  #  modules = [ ./hosts/gungnir/configuration.nix ];
+  #};
 }
