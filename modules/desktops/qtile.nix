01b01755d432f9e6fcdddb12b544b2cd5fbae146
Moskas <minemoskas@gmail.com>
2024-07-03 22:50:34 +0200

    Flake bump, updated qtile options

--- modules/desktops/qtile.nix
+++ modules/desktops/qtile.nix
@@ -1,12 +1,11 @@
 {
-
   services.xserver.windowManager.qtile = {
     enable = true;
-    backend = "x11";
     extraPackages =
       python3Packages: with python3Packages; [
         aria2p
         mpd2
+        hy
       ];
   };
 }
